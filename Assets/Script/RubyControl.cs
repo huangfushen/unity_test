2133ce3febd50fe85f989ee2f9b9792327df83e5
huangfs <huangfs@woobest.com>
2021-09-09 21:16:25 +0800

    first commit

---
+++ Assets/Script/RubyControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //游戏运行开始前调用的方法
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //  
+    void Update()
+    {
+      Vector2 position = transform.position;
+      float speed = 0.1f;
+      if (position.x >= 3)
+      {
+        speed = -0.1f;
+      }
+      position.x = position.x + speed;
+  
+      transform.position  = position;
+    }
+}
