94cec7856edce8151439391259ae9e30fa8b4da2
huangfs <1213885869@qq.com>
2021-10-02 11:01:50 +0800

    新增角色碰撞、血量

---
+++ Assets/Script/HealthCollectible.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        Debug.Log("发生了碰撞");
+    }
+}
